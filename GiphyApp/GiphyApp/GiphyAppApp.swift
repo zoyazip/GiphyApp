7c27b0cd5bbdaee02762440b255811dbe46a3547
Denis Chernov <chernoff.cs@gmail.com>
2023-12-23 14:50:09 +0200

    Init project

---
+++ GiphyApp/GiphyApp/GiphyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiphyAppApp.swift
+//  GiphyApp
+//
+//  Created by Denis Chernovs on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GiphyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
